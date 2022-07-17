2d14f47fc40e73dc3830ef14a7ec8415fb99a93d
jpngeekudin <udinshalah12@gmail.com>
2022-07-17 08:11:44 +0700

    home hot collections

--- src/components/CardNft.tsx
+++ src/components/CardNft.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
+import IconHot from 'assets/images/navbar/navbar-explore-hot-collections.svg';
 
 interface CardNftProps {
-  height?: number
+  height?: number,
+  hot?: boolean
 }
 
-export default function CardNft({ height = 350 }: CardNftProps) {
+export default function CardNft({ height = 350, hot = false }: CardNftProps) {
   return (
-    <div className='rounded' style={{ boxShadow: '0px 0px 18px 0px rgba(230,230,230,1)' }}>
+    <div className='rounded position-relative' style={{ boxShadow: '0px 0px 18px 0px rgba(230,230,230,1)' }}>
+      {hot && (
+        <div className='position-absolute' style={{ top: 20, right: 20 }}>
+          <img src={IconHot} alt="" style={{ height: 18 }} />
+        </div>
+      )}
       <img src="https://picsum.photos/1000" alt="" className='rounded-top w-100' style={{ height, objectFit: 'cover' }} />
       <div className='rounded-bottom d-flex align-items-center p-3' style={{ gap: 10 }}>
         <img src="https://picsum.photos/200" alt="" className='rounded-circle' style={{ objectFit: 'cover', height: 38 }} />
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,14 @@
 import CardNft from 'components/CardNft'
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Slider from 'react-slick';
 
 export default function HomePage() {
   return (
     <div className='container pt-4'>
       <Navbar/>
 
-      <section style={{ marginTop: '2rem' }}>
+      <section style={{ marginTop: '2rem', marginBottom: '2rem' }}>
         <div className="d-flex align-items-center justify-content-between">
           <div style={{ width: 450 }}>
             <div className="text-primary mb-2" style={{ fontSize: '1.8rem', lineHeight: 1, fontWeight: 900 }}>Discover, collect, and sell an Authentic Unique NFTs</div>
@@ -21,6 +22,20 @@ export default function HomePage() {
             <CardNft/>
           </div>
         </div>
+      </section>
+
+      <section style={{ marginBottom: '2rem' }}>
+        <div className="d-flex align-items-center mb-3" style={{ gap: 20 }}>
+          <div className="fw-bold" style={{ fontSize: '1.5rem' }}>Hot Collections</div>
+          <button className="btn btn-primary px-3 rounded-pill">See All Collections</button>
+        </div>
+        <Slider arrows dots={false} infinite slidesToShow={3} slidesToScroll={1}>
+          {[0,1,2,3,4,5].map(i => (
+            <div className='px-3'>
+              <CardNft hot/>
+            </div>
+          ))}
+        </Slider>
       </section>
     </div>
   )
